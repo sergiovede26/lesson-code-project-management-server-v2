b90447045f43ab0cd3a02b2132abb6b5cfa27596
Sergio <sadiazcaballero@gmail.com>
2024-05-24 11:33:01 +0200

    "crating rules for autorization of the login with the email and password"

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,6 +9,30 @@ const User = require("../models/User.model");
 router.post("/signup", (req, res, next) => {
 
   const {email, password, name} = req.body;
+  if (email === "" || password === "" || name === "") {
+    res.status(400).json({message: "please provide all the required information"})
+    return;
+  }
+
+
+// Use regex to validate the email format
+// Regular expressions / regex
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+
+  if (!emailRegex.test(email)) {
+    res.status(400).json({ message: 'Provide a valid email address.' });
+    return;
+  }
+  
+
+// Use regex to validate the password format
+const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+if (!passwordRegex.test(password)) {
+  res.status(400).json({ message: 'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.' });
+  return;
+}
+
+
 
   // Create salt
 
@@ -25,7 +49,13 @@ router.post("/signup", (req, res, next) => {
     name
   }
 
-  User.create(newUser)
+  User.findOne({email: email})
+  .then(foundUser => {
+    if(foundUser){
+      res.status(400).json({message: "email already used"});
+    }
+
+    User.create(newUser)
     .then( createdUser => {
 
       const {email, name, _id} = createdUser;
@@ -38,6 +68,11 @@ router.post("/signup", (req, res, next) => {
       console.log("Error creating new user:", err);
       res.status(500).json({message: "Error creating user"});
     })
+
+
+  })
+  .catch()
+
 
   // res.json({ message: "hello world" });
 })
