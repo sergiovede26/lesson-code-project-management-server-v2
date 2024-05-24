d9573322fe1dce189857c95719001ddafa8feb6e
Sergio <sadiazcaballero@gmail.com>
2024-05-24 11:44:12 +0200

    Auth.routes updated with explanation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,81 +1,79 @@
 const express = require("express");
-const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-//importing user model
+
 const User = require("../models/User.model");
 
-// POST /auth/signup
-router.post("/signup", (req, res, next) => {
+const router = express.Router();
+const saltRounds = 10;
 
-  const {email, password, name} = req.body;
-  if (email === "" || password === "" || name === "") {
-    res.status(400).json({message: "please provide all the required information"})
+
+
+// POST /auth/signup
+router.post('/signup', (req, res, next) => {
+  
+  const { email, password, name } = req.body;
+
+  // Check if the email or password or name is provided as an empty string 
+  if (email === '' || password === '' || name === '') {
+    res.status(400).json({ message: "Provide email, password and name" });
     return;
   }
 
-
-// Use regex to validate the email format
-// Regular expressions / regex
+  // Use regex to validate the email format
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
-
   if (!emailRegex.test(email)) {
     res.status(400).json({ message: 'Provide a valid email address.' });
     return;
   }
-  
 
-// Use regex to validate the password format
-const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-if (!passwordRegex.test(password)) {
-  res.status(400).json({ message: 'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.' });
-  return;
-}
-
-
-
-  // Create salt
-
-  const saltRounds = 10;
-  const salt = bcrypt.genSaltSync(saltRounds);
-
-  // Create hash
-  const hashedPassword = bcrypt.hashSync(password, salt);
-
-
-  const newUser = {
-    email,
-    password: hashedPassword,
-    name
+  // Use regex to validate the password format
+  const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!passwordRegex.test(password)) {
+    res.status(400).json({ message: 'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.' });
+    return;
   }
 
-  User.findOne({email: email})
-  .then(foundUser => {
-    if(foundUser){
-      res.status(400).json({message: "email already used"});
-    }
 
-    User.create(newUser)
-    .then( createdUser => {
+  // Check the users collection if a user with the same email already exists
+  User.findOne({ email })
+    .then((foundUser) => {
+      // If the user with the same email already exists, send an error response
+      if (foundUser) {
+        res.status(400).json({ message: "User already exists." });
+        return;
+      }
 
-      const {email, name, _id} = createdUser;
+      // If the email is unique, proceed to hash the password
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashedPassword = bcrypt.hashSync(password, salt);
 
+      const newUser = {
+        email: email,
+        password: hashedPassword,
+        name: name
+      };
+
+      // Create a new user in the database
+      // We return a pending promise, which allows us to chain another `then` 
+      return User.create(newUser);
+    })
+    .then((createdUser) => {
+      // Deconstruct the newly created user object to omit the password
+      // We should never expose passwords publicly
+      const { email, name, _id } = createdUser;
+
+      // Create a new object that doesn't expose the password
       const user = { email, name, _id };
 
+      // Send a json response containing the user object
       res.status(201).json({ user: user });
     })
     .catch(err => {
-      console.log("Error creating new user:", err);
-      res.status(500).json({message: "Error creating user"});
-    })
-
-
-  })
-  .catch()
-
-
-  // res.json({ message: "hello world" });
-})
+      console.log(err);
+      res.status(500).json({ message: "Internal Server Error" })
+    });
+});
 
 
 module.exports = router;
