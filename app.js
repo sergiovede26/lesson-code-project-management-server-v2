aff34fba9e3655c94a3e2d93761009ba5b118335
Sergio <sadiazcaballero@gmail.com>
2024-05-24 15:04:29 +0200

    Last update 24.05.2024

--- app.js
+++ app.js
@@ -2,21 +2,19 @@ require("dotenv/config");
 require("./db");
 const express = require("express");
 
+
 const app = express();
 
 require("./config")(app);
 
-// 👇 Start handling routes here
-const allRoutes = require("./routes");
-app.use("/api", allRoutes);
 
 
-
-//👇 Mounting routes
+//
+// Mount routes
+//
 app.use("/auth", require("./routes/auth.routes"));
 app.use("/api", require("./routes/project.routes"));
 app.use("/api", require("./routes/task.routes"));
-
 
 
 require("./error-handling")(app);
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 const User = require("../models/User.model");
 
@@ -9,9 +12,10 @@ const saltRounds = 10;
 
 
 
-// POST /auth/signup
+
+// POST /auth/signup - Register
 router.post('/signup', (req, res, next) => {
-  
+
   const { email, password, name } = req.body;
 
   // Check if the email or password or name is provided as an empty string 
@@ -74,6 +78,70 @@ router.post('/signup', (req, res, next) => {
       res.status(500).json({ message: "Internal Server Error" })
     });
 });
+
+
+
+// POST  /auth/login - Verifies email and password and returns a JWT
+router.post('/login', (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Check if email or password are provided as empty string 
+  if (email === '' || password === '') {
+    res.status(400).json({ message: "Provide email and password." });
+    return;
+  }
+
+  // Check the users collection if a user with the same email exists
+  User.findOne({ email })
+    .then((foundUser) => {
+
+      if (!foundUser) {
+        // If the user is not found, send an error response
+        res.status(401).json({ message: "User not found." })
+        return;
+      }
+
+      // Compare the provided password with the one saved in the database
+      const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
+
+      if (passwordCorrect) {
+        // Deconstruct the user object to omit the password
+        const { _id, email, name } = foundUser;
+
+        // Create an object that will be set as the token payload
+        const payload = { _id, email, name };
+
+        // Create and sign the token
+        const authToken = jwt.sign(
+          payload,
+          process.env.TOKEN_SECRET,
+          { algorithm: 'HS256', expiresIn: "6h" }
+        );
+
+        // Send the token as the response
+        res.status(200).json({ authToken: authToken });
+      }
+      else {
+        res.status(401).json({ message: "Unable to authenticate the user" });
+      }
+
+    })
+    .catch(err => res.status(500).json({ message: "Internal Server Error" }));
+});
+
+
+// GET  /auth/verify  -  Used to verify JWT stored on the client
+router.get('/verify', isAuthenticated, (req, res, next) => {       // <== CREATE NEW ROUTE
+ 
+  // If JWT token is valid the payload gets decoded by the
+  // isAuthenticated middleware and made available on `req.payload`
+  console.log(`req.payload`, req.payload);
+ 
+  // Send back the object with user data
+  // previously set as the token payload
+  res.json(req.payload);
+});
+
 
 
 module.exports = router;
